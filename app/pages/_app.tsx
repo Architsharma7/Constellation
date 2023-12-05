9febb0da96eaeb92507636911b1696807456df3a
nick-w3 <nick-w3@nick-w3s-MacBook-Pro.local>
2023-12-05 17:06:55 +0200

    contract interactions + apis for topk

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -9,19 +9,19 @@ import {
   lightTheme,
 } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { polygonMumbai } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { useRouter } from "next/router";
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [polygonMumbai],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "ad9d4173328447d73a95b113fec565eb",
   chains,
 });
 
